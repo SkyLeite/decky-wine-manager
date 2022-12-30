ac99ada0dee3a54a38b4c39d491b078c58732bd1
Sky Leite <sky@leite.dev>
2022-12-30 20:26:50 -0300

    WIP

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import {
   ServerAPI,
   staticClasses,
 } from "decky-frontend-lib";
-import { VFC } from "react";
+import { useMemo, VFC } from "react";
 import { FaCheck, FaShip, FaSpinner } from "react-icons/fa";
 import AppContext from "./context";
 import { useProtonInstalls, useShowReleaseList } from "./hooks";
@@ -32,6 +32,20 @@ const Manage: VFC = () => {
 
 const VersionList: VFC = () => {
   const protonInstalls = useProtonInstalls();
+  const installsByStatus = useMemo(() => {
+    const initialState = { installed: [], installing: [] };
+    if (!protonInstalls.data) return initialState;
+
+    return protonInstalls.data.reduce((acc, current) => {
+      if (acc[current.status]) {
+        acc[current.status].push(current);
+      } else {
+        acc[current.status] = [current];
+      }
+
+      return acc;
+    }, initialState);
+  }, [protonInstalls.isFetched]);
 
   if (protonInstalls.isError) {
     return <span>{protonInstalls.error}</span>;
@@ -39,27 +53,46 @@ const VersionList: VFC = () => {
 
   if (protonInstalls.isSuccess) {
     return (
-      <PanelSection title="Installs">
-        {protonInstalls.data.map((proton) => (
-          <PanelSectionRow>
-            <ButtonItem layout="below">
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                }}
-              >
-                <span>{proton.name}</span>
-                {proton.status == "installed" && (
-                  <FaCheck style={{ display: "block" }} />
-                )}
-                {proton.status == "installing" && <Spinner />}
-              </div>
-            </ButtonItem>
-          </PanelSectionRow>
-        ))}
-      </PanelSection>
+      <div>
+        {installsByStatus.installing.length > 0 && (
+          <PanelSection title="Installing">
+            {installsByStatus.installing.map((install) => (
+              <PanelSectionRow>
+                <ButtonItem layout="below">
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "space-between",
+                    }}
+                  >
+                    <span>{install.name}</span>
+                    <Spinner />
+                  </div>
+                </ButtonItem>
+              </PanelSectionRow>
+            ))}
+          </PanelSection>
+        )}
+
+        <PanelSection title="Installed">
+          {installsByStatus.installed.map((install) => (
+            <PanelSectionRow>
+              <ButtonItem layout="below">
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <span>{install.name}</span>
+                </div>
+              </ButtonItem>
+            </PanelSectionRow>
+          ))}
+        </PanelSection>
+      </div>
     );
   }
 
